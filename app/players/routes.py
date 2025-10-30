8924dee18b84dbb6bf3389a45d34e6a1ac7bd081
Travis Agne <travis.agne.98@gmail.com>
2025-10-29 19:51:56 -0500

    1.0 updated routes to pull in nfl_data records, TODO parse data from json and organize

--- app/players/routes.py
+++ app/players/routes.py
@@ -2,14 +2,16 @@ from flask import Flask
 from flask_restful import Resource
 from .players_bp import players_api
 
-players = {
-    1: 'player1',
-    2: 'player2',
-    3: 'player3'
-}
+import nfl_data_py as nfl_data
+import pandas as pd
+import numpy as np
 
 class Players(Resource):
-    def get(self, id):
-        return players[id], 200
+    def get(self, season):
+        
+        result = nfl_data.import_seasonal_data([2024], 'REG')
+        json = result.to_json()
+        return json, 200 
 
-players_api.add_resource(Players, '/get/<int:id>')   
+
+players_api.add_resource(Players, '/get/<int:season>')   
